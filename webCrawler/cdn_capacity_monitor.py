4ac8c42fbed2be9d70910f393d089d0940bd189d
CMCCxuSir <yxu9428@163.com>
2018-08-16 13:27:31 +0800

    fenghuo OTT fixed

--- webCrawler/cdn_capacity_monitor.py
+++ webCrawler/cdn_capacity_monitor.py
@@ -31,7 +31,7 @@ for item in f.split("\""):
             maximum = float(item)
 
 # print(maximum)
-print("%.2f" % (maximum/1024/1024/1024*1.0735))
+print("%.2f" % (maximum/1024/1024/1024))
 
 url = 'http://39.134.89.13:3000/api/datasources/proxy/1/api/v1/query_range?query=sum(irate(node_network_transmit_bytes%7Bgroup%3D%22%E6%9D%A8%E6%B5%A6%E8%BE%B9%E7%BC%98%E8%8A%82%E7%82%B9%22%2Cdevice%3D~%22%5Elo%7Cbond0%7Cbond1%22%7D%5B5m%5D))%20%20*%208&start=' + ts + '&end=' + te + '&step=240'
 f = webCrawler.webcrawler.get_web_page(url)
@@ -43,7 +43,7 @@ for item in f.split("\""):
             maximum = float(item)
 
 # print(maximum)
-print("%.2f" % (maximum/1024/1024/1024*1.0735))
+print("%.2f" % (maximum/1024/1024/1024))
 
 
 # part2 华为
