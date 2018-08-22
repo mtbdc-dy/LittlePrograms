71ef5f1ded468f4dfd2e5bbe056604434c9c7f62
CMCCxuSir <yxu9428@163.com>
2018-08-23 05:17:25 +0800

    two new employee tests

---
+++ 测试/break.py
@@ -0,0 +1,14 @@
+count = 0
+
+for i in range(0,5):
+    if i == 2:
+        break
+
+print(i)
+
+i = '12'
+print(i[0:3])
+
+i = '4-8613515200000           11         11         8'
+
+print(i.split())
---
+++ 测试/excel.py
@@ -0,0 +1,20 @@
+import xlrd
+
+filename_output = '附件五、物联网HLR-ID（截至2018年2月）.xlsx'
+
+f3 = xlrd.open_workbook(filename_output)
+table = f3.sheet_by_name("Sheet1")
+nrows = table.nrows
+for i in range(nrows):
+    rows  = table.row_values(i)
+    # print(rows[0:1])
+
+str = '123435'
+print(str[0:2])
+
+matrix = [[] for i in range(10)]
+
+matrix[1].append('1')
+matrix[1].append('1')
+matrix[2].append('1')
+print(matrix)
