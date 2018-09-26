53d9e4b6d758dfdbaa9ab763be532ba33d2af0e7
CMCCxuSir <yxu9428@163.com>
2018-09-26 17:05:21 +0800

    loop

--- 测试/callable.py
+++ 测试/callable.py
@@ -3,8 +3,12 @@ import math
 
 # callable测试
 print(callable(math.sqrt))
-
-for i in range(1, 9):
+a = 1
+b = 2
+if (a + b == 3) or (a - b == 1):
+    # print('asda')
+    print()
+for i in [1, 2, 3]:
     print(i)
 
 
