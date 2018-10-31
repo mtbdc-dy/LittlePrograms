21489b61494f6e368490e6556b809acaf3da32a1
CMCCxuSir <yxu9428@163.com>
2018-10-31 14:01:29 +0800

    cdn monitor modified 10/31

--- web/webCrawler/cdn_capacity_monitor2.py
+++ web/webCrawler/cdn_capacity_monitor2.py
@@ -25,8 +25,16 @@ OTT、IPTV 流量统计四部部分组成：
 """
 
 '''Constants'''
-IPTV_total_capacity = 675+72+72
-OTT_total_capacity = 850-95-72-72
+# IPTV
+IPTV_total_capacity = 675+72+72     # 819G
+# OTT
+FX_FengHuo_OTT = 242
+YP_FengHuo_OTT = 90
+
+FX_HuaWei_OTT = 108
+PDandJS_HuaWei_OTT = 170
+
+OTT_total_capacity = FX_FengHuo_OTT + YP_FengHuo_OTT + FX_HuaWei_OTT + PDandJS_HuaWei_OTT  # 610G
 file_output = 'cdn_rate.csv'  # 输出文件
 
 # 打开输出文件
