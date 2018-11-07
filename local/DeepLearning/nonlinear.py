0983004c392e7af65e45c820f63adbeaacf6f1c9
CMCCxuSir <yxu9428@163.com>
2018-11-07 22:43:21 +0800

    DL

--- local/DeepLearning/nonlinear.py
+++ local/DeepLearning/nonlinear.py
@@ -11,6 +11,7 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def add_layer(inputs, in_size, out_size, activation_function=None):
     Weights = tf.Variable(tf.random_normal([in_size, out_size]))
     biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
