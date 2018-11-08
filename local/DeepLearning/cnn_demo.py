42012533c500604125a309e5966be6963cf1c1fb
CMCCxuSir <yxu9428@163.com>
2018-11-08 22:24:04 +0800

    DL

--- local/DeepLearning/cnn_demo.py
+++ local/DeepLearning/cnn_demo.py
@@ -12,6 +12,7 @@ from tensorflow.examples.tutorials.mnist import input_data
 # number 1 to 10 data
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
+
 def compute_accuracy(v_xs, v_ys):
     global prediction
     y_pre = sess.run(prediction, feed_dict={xs: v_xs, keep_prob: 1})
@@ -20,22 +21,27 @@ def compute_accuracy(v_xs, v_ys):
     result = sess.run(accuracy, feed_dict={xs: v_xs, ys: v_ys, keep_prob: 1})
     return result
 
+
 def weight_variable(shape):
     initial = tf.truncated_normal(shape, stddev=0.1)
     return tf.Variable(initial)
 
+
 def bias_variable(shape):
     initial = tf.constant(0.1, shape=shape)
     return tf.Variable(initial)
+
 
 def conv2d(x, W):
     # stride [1, x_movement, y_movement, 1]
     # Must have strides[0] = strides[3] = 1
     return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
+
 def max_pool_2x2(x):
     # stride [1, x_movement, y_movement, 1]
     return tf.nn.max_pool(x, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+
 
 # define placeholder for inputs to network
 xs = tf.placeholder(tf.float32, [None, 784])/255.   # 28x28
@@ -44,19 +50,19 @@ keep_prob = tf.placeholder(tf.float32)
 x_image = tf.reshape(xs, [-1, 28, 28, 1])
 # print(x_image.shape)  # [n_samples, 28,28,1]
 
-## conv1 layer ##
+# conv1 layer ##
 W_conv1 = weight_variable([5, 5, 1, 32])  # patch 5x5, in size 1, out size 32
 b_conv1 = bias_variable([32])
 h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1) # output size 28x28x32
 h_pool1 = max_pool_2x2(h_conv1)                                         # output size 14x14x32
 
-## conv2 layer ##
+# conv2 layer ##
 W_conv2 = weight_variable([5,5, 32, 64]) # patch 5x5, in size 32, out size 64
 b_conv2 = bias_variable([64])
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2) # output size 14x14x64
 h_pool2 = max_pool_2x2(h_conv2)                                         # output size 7x7x64
 
-## fc1 layer ##
+# fc1 layer ##
 W_fc1 = weight_variable([7*7*64, 1024])
 b_fc1 = bias_variable([1024])
 # [n_samples, 7, 7, 64] ->> [n_samples, 7*7*64]
