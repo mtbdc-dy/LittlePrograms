d43ef5319e72c0af0e701a19fa10a07623972d8b
CMCCxuSir <yxu9428@163.com>
2018-12-28 12:08:14 +0800

    cdn_rate.csv

--- web/webCrawler/cdn_capacity_monitor_utm_chrome.py
+++ web/webCrawler/cdn_capacity_monitor_utm_chrome.py
@@ -1,14 +1,11 @@
 from selenium import webdriver
-from selenium.webdriver import Firefox
-from selenium.webdriver.support.events import EventFiringWebDriver, AbstractEventListener
+from selenium.webdriver import Chrome
 from selenium.webdriver.common.keys import Keys
 # The Keys class provide keys in the keyboard like RETURN, F1, ALT etc.
+from selenium.webdriver.common.by import By
 
-profile = webdriver.FirefoxProfile('6wg635ft.default')
-profile.accept_untrusted_certs = True
-
-driver = Firefox(firefox_profile=profile)
-driver.get("https://39.134.87.216:31943/itpaas/login.action")
+driver = Chrome()
+driver.get("https://39.134.87.216:31943/pm/themes/default/pm/app/i2000_monitorView_pm.html?curMenuId=com.iemp.app.pm.monitorView&_=1545967221368#group_152734715982719")
 # print(driver.page_source)
 usr = driver.find_element_by_xpath("//*[@id=\"username\"]")
 usr.send_keys("admin")
@@ -18,5 +15,8 @@ captcha = driver.find_element_by_xpath("//*[@id=\"validate\"]")
 vc = input('输入网页上的验证码')
 captcha.send_keys(vc)
 captcha.send_keys(Keys.RETURN)
-# assert "No results found." not in driver.page_source
+print(driver.get_cookies())
+button = driver.find_element_by_css_selector('#treeDiv_1_a')
+button.click()
+
 # driver.close()
