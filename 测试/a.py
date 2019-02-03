0d1696d16dffd6a7cb8c13a5f213f6c3fb0eb109
CMCCxuSir <yxu9428@163.com>
2019-02-03 17:01:43 +0800

    cdn rate

--- 测试/a.py
+++ 测试/a.py
@@ -3,21 +3,26 @@ import myPackages.process_txt as mp
 import os
 import sys
 
-"""
-    path 相关
-"""
-
+# 当前文件绝对路径
 print(os.path.dirname(os.path.abspath(__file__)))
+
 print(int(1/2))
+mp.show_path()  # D:\PyProjects\LittlePrograms\测试
 a = 1
 b = 2
 # a, b = b, a
 b, a = a, b
 print(a, b)
-
-
-print(pow(10, -0.7))
-
-mp.show_path()  # D:\PyProjects\LittlePrograms\测试
-
 print(os.getcwd())
+print(pow(10, -0.5))
+
+a = [1, 2, 0]
+if all(a):
+    print('True')
+
+
+
+
+
+
+
