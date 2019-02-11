65fa53ad7a8824753d033fe6c263ee98ec3495e9
CMCCxuSir <yxu9428@163.com>
2019-02-11 12:46:37 +0800

    cdn rate

--- web/communicate_with_ct_devices/helloworld_paramiko.py
+++ web/communicate_with_ct_devices/helloworld_paramiko.py
@@ -1,4 +1,6 @@
 import paramiko
+import chardet
+
 
 # 创建SSH对象
 ssh = paramiko.SSHClient()
@@ -8,10 +10,12 @@ ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh.connect('117.185.13.195', 22, 'llz@shmc', 'sw_76@Shmc')
 
 # 执行命令
-stdin, stdout, stderr = ssh.exec_command('dis int')
+stdin, stdout, stderr = ssh.exec_command('dis arp')
 # 获取命令结果
-print(stdout.read())
-
+# print(type(stdout.read()))
+print(str(stdout.read()))
+ret = chardet.detect(stdout.read())
+print(ret)
 # 关闭连接
 ssh.close()
 
