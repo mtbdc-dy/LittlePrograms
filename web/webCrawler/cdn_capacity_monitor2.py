6f6e18abd66252e3b39d7f2c9aa267492102db9e
Yuan XU <Yxu9428@163.com>
2019-02-17 20:55:29 +0800

    cdn_rate

--- web/webCrawler/cdn_capacity_monitor2.py
+++ web/webCrawler/cdn_capacity_monitor2.py
@@ -318,6 +318,7 @@ laggy_device_ratio = 100 - (float(tmp_normal_device)/(float(tmp_normal_device)+f
 
 
 # SQM取NEI相关
+# SQM取NEI相关
 def sqm_nei(cookie):
     url = 'http://117.144.107.165:8088/evqmaster/report/reportaction!returnMiguData.action'
     form = {
@@ -325,34 +326,53 @@ def sqm_nei(cookie):
                      ': 4, \"dimension\": \"platform\", \"platform\": \"\", \"tType\": 2, \"isMigu\": false, \"isMiguShanxi'
                      '\": false, \"bIncludeShanxi\": false}'
     }
-
     f = ww.post_web_page(url, form, cookie)
-    fc = f
-    # EPG响应时长
-    tmp_index = f.find('CntEpgRspTime')
-    f = f[tmp_index:]
-    CntEpgRspTime = f[f.find(':') + 1:f.find(',')]
-    CntEpgRspTime = float(CntEpgRspTime)
-    tmp_index = f.find('TotEpgRspTime')
-    f = f[tmp_index:]
-    TotEpgRspTime = f[f.find(':') + 1:f.find(',')]
-    TotEpgRspTime = float(TotEpgRspTime)
-    epg_latency = TotEpgRspTime/CntEpgRspTime / 1000000
+    tmp_dict = json.loads(f)
+    sqm_dict = json.loads(tmp_dict['resultData'])
+    # print(sqm_dict.keys())
+    fault = {0, 1, 2, 3, 4, 5, 6, 7, 8, 14, 15}
+    list_count = list()
+    list_total = list()
+    for i in fault:
+        count_tmp = 0
+        total_tmp = 0
+        for item in sqm_dict['vod']:
+            if item['FaultID'] == i:
+                count_tmp += item['Count']
+                total_tmp += item['Total']
+        list_count.append(count_tmp)
+        list_total.append(total_tmp)
+    # print(list_total)
+    # print(list_count)
 
-    # EPG响应成功率
-    tmp_index = fc.find('Requests')
-    fc = fc[tmp_index:]
-    Requests = fc[fc.find(':') + 1:fc.find(',')]
-    Requests = float(Requests)
-    tmp_index = fc.find('Responses')
-    fc = fc[tmp_index:]
-    Responses = fc[fc.find(':') + 1:fc.find(',')]
-    Responses = float(Responses)
-    epg_success_ratio = Responses / Requests * 100
-    return epg_latency, epg_success_ratio
+    # 卡顿时间占比 = 卡顿时长 / 下载时长
+    lag_duration = list_total[2] / 1000000
+    total_duration = list_total[6]
+    print('卡顿时间占比:', round(lag_duration / total_duration * 100, 2))
+
+    # 卡顿次数占比 = 15 / 7 * 100
+    lag_times = list_total[10]
+    total_times = list_total[7]  # 播放节目数
+    print('卡顿次数占比:', round(lag_times / total_times * 100, 2))
+
+    # 首帧缓冲时长
+    print('首帧缓冲时长(S):', round(list_total[4] / list_count[4] / 1000000, 2))  # 秒
+
+    # 电视播放成功率
+    print('电视播放成功率', round(list_total[8] / list_total[7] * 100, 2))
+
+    # EPG加载成功率 加载成功率 和 登录成功率
+    # print(sqm_dict['epg'])
+    # print(sqm_dict['epgSuc'])
+    print('EPG加载成功率', round(sqm_dict['epg'][0]['Responses'] / sqm_dict['epg'][0]['Requests'] * 100, 2))
+
+    # 卡顿时间占比, 卡顿次数占比, 首帧缓冲时长, 电视播放成功率, EPG加载成功率
+    return (round(lag_duration / total_duration * 100, 2), round(lag_times / total_times * 100, 2),
+            round(list_total[4] / list_count[4] / 1000000, 2), round(list_total[8] / list_total[7] * 100, 2),
+            round(sqm_dict['epg'][0]['Responses'] / sqm_dict['epg'][0]['Requests'] * 100, 2))
 
 
-epg_latency, epg_success_ratio = sqm_nei(cookie)
+lag_time_proportion, lag_count_proportion, first_frame_latency, tv_success_ratio, epg_success_ratio = sqm_nei(cookie)
 print()
 
 '''partX CMNET出口数据统计报表'''
@@ -421,7 +441,8 @@ csv_content = [startTime] + ['{:.2f}'.format(maxStreamSTBs/10000)] + ['{:.2f}'.f
               ['%.2f' % ott_mean_rate] + ['{:.2f}'.format(ott_max_rate/OTT_total_capacity*100)] +\
               ['{:.2f}'.format(max_user/10000)] + ['{:.2f}'.format(max_rate)] + ['{:.2f}'.format(mean_rate)] +\
               ['{:.2f}'.format(max_rate/IPTV_total_capacity*100)] + ['{:.2f}'.format(laggy_device_ratio)] +\
-              [sum_box] + ['%.2f' % epg_success_ratio] + ['%.2f' % epg_latency]
+              [sum_box] + [lag_time_proportion] + [lag_count_proportion] + [first_frame_latency] + [tv_success_ratio] +\
+              [epg_success_ratio]
 print('email_content: ', email_content)
 # print('csv_content:', csv_content)
 user = [
--- web/webCrawler/cdn_capacity_sqm.py
+++ web/webCrawler/cdn_capacity_sqm.py
@@ -30,48 +30,53 @@ def sqm_nei(cookie):
                      ': 4, \"dimension\": \"platform\", \"platform\": \"\", \"tType\": 2, \"isMigu\": false, \"isMiguShanxi'
                      '\": false, \"bIncludeShanxi\": false}'
     }
-
-
     f = ww.post_web_page(url, form, cookie)
-    # print(f)
-    nei_json = json.loads(f)
+    tmp_dict = json.loads(f)
+    sqm_dict = json.loads(tmp_dict['resultData'])
+    # print(sqm_dict.keys())
+    fault = {0, 1, 2, 3, 4, 5, 6, 7, 8, 14, 15}
+    list_count = list()
+    list_total = list()
+    for i in fault:
+        count_tmp = 0
+        total_tmp = 0
+        for item in sqm_dict['vod']:
+            if item['FaultID'] == i:
+                count_tmp += item['Count']
+                total_tmp += item['Total']
+        list_count.append(count_tmp)
+        list_total.append(total_tmp)
+    # print(list_total)
+    # print(list_count)
 
-    # print(nei_json)
-    # print(nei_json.keys())
-    # print(nei_json['resultData'])
-    # print(type(nei_json['resultData']))
-    data_json = json.loads(nei_json['resultData'])
-    # print(nei_json)
-    print(data_json['vod'])
+    # 卡顿时间占比 = 卡顿时长 / 下载时长
+    lag_duration = list_total[2] / 1000000
+    total_duration = list_total[6]
+    print('卡顿时间占比:', round(lag_duration / total_duration * 100, 2))
 
+    # 卡顿次数占比 = 15 / 7 * 100
+    lag_times = list_total[10]
+    total_times = list_total[7]  # 播放节目数
+    print('卡顿次数占比:', round(lag_times / total_times * 100, 2))
 
-    # EPG响应时长
-    return
+    # 首帧缓冲时长
+    print('首帧缓冲时长(S):', round(list_total[4] / list_count[4] / 1000000, 2))  # 秒
 
-    # fc = f
-    # # EPG响应时长
-    # tmp_index = f.find('CntEpgRspTime')
-    # f = f[tmp_index:]
-    # CntEpgRspTime = f[f.find(':') + 1:f.find(',')]
-    # CntEpgRspTime = float(CntEpgRspTime)
-    # tmp_index = f.find('TotEpgRspTime')
-    # f = f[tmp_index:]
-    # TotEpgRspTime = f[f.find(':') + 1:f.find(',')]
-    # TotEpgRspTime = float(TotEpgRspTime)
-    # epg_latency = TotEpgRspTime/CntEpgRspTime / 1000000
-    #
-    # # EPG响应成功率
-    # tmp_index = fc.find('Requests')
-    # fc = fc[tmp_index:]
-    # Requests = fc[fc.find(':') + 1:fc.find(',')]
-    # Requests = float(Requests)
-    # tmp_index = fc.find('Responses')
-    # fc = fc[tmp_index:]
-    # Responses = fc[fc.find(':') + 1:fc.find(',')]
-    # Responses = float(Responses)
-    # epg_success_ratio = Responses / Requests * 100
-    # return epg_latency, epg_success_ratio
+    # 电视播放成功率
+    print('电视播放成功率', round(list_total[8] / list_total[7] * 100, 2))
+
+    # EPG访问成功率 加载成功率 和 登录成功率
+    print(sqm_dict['epg'])
+    print(sqm_dict['epgSuc'])
+    print('EPG加载成功率', sqm_dict['epg'][0]['Responses'] / sqm_dict['epg'][0]['Requests'] * 100)
+    # print(sqm_dict['epgSuc'][0]['Responses'] / sqm_dict['epgSuc'][0]['Requests'])
+
+    # 卡顿时间占比, 卡顿次数占比, 首帧缓冲时长, 电视播放成功率, EPG加载成功率
+    return (round(lag_duration / total_duration * 100, 2), round(lag_times / total_times * 100, 2),
+            round(list_total[4] / list_count[4] / 1000000, 2), round(list_total[8] / list_total[7] * 100, 2),
+            round(sqm_dict['epg'][0]['Responses'] / sqm_dict['epg'][0]['Requests'] * 100, 2))
 
 
 cookie = wl.sqm_117()
-epg_latency, epg_success_ratio = sqm_nei(cookie)
+epg_latency, epg_success_ratio, a, b = sqm_nei(cookie)
+print(epg_latency, epg_success_ratio, a, b)
