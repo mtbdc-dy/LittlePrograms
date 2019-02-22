071245c45ddc8e220467cc83eb866b5efc66da2e
CMCCxuSir <yxu9428@163.com>
2019-02-22 16:13:01 +0800

    cdn rate

--- 测试/re_hello.py
+++ 测试/re_hello.py
@@ -1,2 +1,15 @@
 import re
+f = open('20190213 SHSW01BER', 'rb')
+line = f.readlines()
+for item in line:
+    if b'interfaces' in item:
+        print(item)
+print(type(line))
+
+exit()
+Line = '123ge-11/2/9asd'
+m = re.search(r'[A-Za-z]*-\d*/\d*/\d*', Line)
+# \w
+# 匹配包括下划线的任何单词字符。类似但不等价于“[A-Za-z0-9_]”，这里的"单词"字符使用Unicode字符集
+print(m.group(0))
 
