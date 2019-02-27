be609435cfd90f91085f4785987709ba1aa0e584
CMCCxuSir <yxu9428@163.com>
2019-02-27 17:05:37 +0800

    cdn rate

--- 测试/a.py
+++ 测试/a.py
@@ -1,7 +1,45 @@
-import math
+import matplotlib.pyplot as plt
+from PIL import Image
+import threading
+
 import myPackages.process_txt as mp
 import os
 import sys
+
+picture_path = 'validateCode.jpeg'
+s = '中兴'
+S = s.encode('GBK')
+print(S)
+x = b'\xd6\xd0\xd0\xcb\x69\x43\x61\x63\x68\x65\x3d\xd6\xd0\xd0\xcb\xbb\xba\xb4\xe6\xbc\xd3\xcb\xd9\xcd\xf8\xc2\xe7'
+X = x.decode('GBK')
+print(X)
+exit()
+def part1():
+    img = plt.imread(picture_path)
+    plt.imshow(img)
+    plt.show()
+    print(img)
+    # plt.ion()
+    return
+
+
+def part2():
+    a = Image.open(picture_path)
+    a.show()
+    return
+
+
+# Part1 plt
+# Part2 Image
+part1()
+part2()
+t = threading.Thread(target=part2)  # 创建线程
+print('111')
+t.setDaemon(True)#设置为后台线程，这里默认是False，设置为True之后则主线程不用等待子线程
+t.start()
+
+part2()
+exit()
 
 # 当前文件绝对路径
 print(os.path.dirname(os.path.abspath(__file__)))
