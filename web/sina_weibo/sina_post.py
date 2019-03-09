700e30c083fb7dfc5d5c816408f3abe8d24f6f31
Yuan XU <Yxu9428@163.com>
2019-03-09 22:19:18 +0800

    cdn_rate

--- web/sina_weibo/sina_post.py
+++ web/sina_weibo/sina_post.py
@@ -14,11 +14,23 @@ def post():
     url_post_pic = 'https://api.weibo.com/2/statuses/share.json'
     playload = {
         "access_token": "2.00ixOpPDIg6znDa273b69da7ZFtzqB",
-        # www.doutula.com 找些傻逼表情包配上
-        "status": "softpedia https://www.softpedia.com",
+        "status": "速度慢问题",    # 微博内容
+    }
+    r = requests.post(url_post_pic, data=playload)
+    # r = requests.post(url_post_pic, data=playload)
+    return r
+
+
+def post_with_pic():
+    url_post_pic = 'https://api.weibo.com/2/statuses/share.json'
+    playload = {
+        "access_token": "2.00ixOpPDIg6znDa273b69da7ZFtzqB",
+
+        "status": "解决stackoverflow.com网站打开速度慢问题 https://blog.csdn.net/weixin_40700486/article/details/83820972#_5",
+    # 微博内容
     }
     files = {
-        "pic": open("20181128360682_mSGega.gif", "rb")
+        "pic": open("1.jpg", "rb")  # www.doutula.com 找些傻逼表情包配上
     }
     r = requests.post(url_post_pic, data=playload, files=files)
     # r = requests.post(url_post_pic, data=playload)
@@ -26,7 +38,8 @@ def post():
 
 
 if __name__ == '__main__':
-    # print(check_token())
+    print(check_token())
+    exit()
     f = post()
     print(f, f.content)
 
