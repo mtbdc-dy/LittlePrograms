18fa2688b43b5f203abfa2359c8056d0ecf7a38e
Yuan XU <Yxu9428@163.com>
2019-03-12 10:53:45 +0800

    cdn_rate

---
+++ local/textProcesing/sqm_api_csv.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/3/12 10:23 AM
+# @Author  : 徐缘
+# @File    : sqm_api_csv.py
+# @Software: PyCharm
+
+
+import csv
+
+filename = 'STB_JMMX_20190312050000_1.CSV'
+filename_output = 'output_cname' + '.csv'
+
+f = open(filename, 'r')  # utf8   gb2312
+reader = csv.reader(f)
+
+g = open(filename_output, 'w')
+writer = csv.writer(g)
+
+for row in reader:
+    print(row[0].split('\x7f'))
+    writer.writerow(row[0].split('\x7f'))
+
