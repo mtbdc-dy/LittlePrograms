1535dadeb53df8d554edab32650177c0100bd667
CMCCxuSir <yxu9428@163.com>
2019-03-18 16:44:20 +0800

    cdn rate

--- web/zte_small_cache/cache_performance_zte.py
+++ web/zte_small_cache/cache_performance_zte.py
@@ -9,8 +9,14 @@ import web.webCrawler.login as wl
 import web.webCrawler.webcrawler as ww
 import json
 
+import csv
+import datetime
 
-def httpstatus():
+
+filename = 'output.csv'
+
+
+def get_httpstatus():
 
     return
 
@@ -21,13 +27,31 @@ def hit_ratio():
 
 
 if __name__ == '__main__':
+    # 打开输出文件，查看日期
+    # f = open(filename, 'r')
+    # print(f)
+    # print(f.readlines()[-1])
+    # exit()
+
+    # 写文件
+    g = open(filename, 'a')
+    writer = csv.writer(g)
+
+    # 日期
+    now = datetime.datetime.now()
+    yesterday = now - datetime.timedelta(days=1)
+
+    begin = yesterday.strftime('%Y-%m-%d')
+    end = now.strftime('%Y-%m-%d')
+
     cookie = wl.zte_cdn_omc()
     print(cookie)
+    # 节点状态码
     url = 'https://39.134.88.198:8443/stat/nodestatuscode_query.action'
     form = {
         'offset': '8',
-        'beginDate': '2019-03-15 00:00:00',
-        'endDate': '2019-03-15 16:32:02',
+        'beginDate': begin + ' 00:00:00',
+        'endDate': end + ' 00:00:00',
         'areaid': '',
         'nodeid': 'SMALLcache',
         'deviceid': '',
@@ -41,9 +65,25 @@ if __name__ == '__main__':
 
     f = ww.post_web_page_ssl(url, form, cookie)
     print(f)
-    json_tmp = json.loads(f)
-    print(json_tmp)
+    tmp_dict = json.loads(f)
 
+    "EDIT SCRIPT FROM HERE"
+    tmp_dict = tmp_dict['message']
+    print(len(tmp_dict))
+    print(tmp_dict[0]['errormessage'][0]['errornum'])
+
+    httpstatus = list()
+    for i in range(5):
+        httpstatus.append(0)
+
+    for item in tmp_dict:
+        if isinstance(item, dict):
+            print(item)
+            for i in range(5):
+                httpstatus[i] += int(item['errormessage'][i]['errornum'])
+
+    # print(httpstatus)
+    writer.write(httpstatus())
 
 
 
