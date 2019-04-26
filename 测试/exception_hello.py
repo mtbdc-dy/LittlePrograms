fd396b91e7b24c5d4e263a1d53022851068421c6
CMCCxuSir <yxu9428@163.com>
2019-04-27 06:36:06 +0800

    cdn rate

---
+++ 测试/exception_hello.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Time : 2019/4/26,026 21:00
+# @Author : 徐缘
+# @FileName: exception_hello.py
+# @Software: PyCharm
+
+
+try:
+    b = 1
+except:
+    print('warning: Tensorflow is not found.')
