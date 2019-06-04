57730ceccc29f758a7c69a335f4a60b140a6ba0e
Yuan XU <Yxu9428@163.com>
2019-06-04 19:39:01 +0800

    cdn_rate

--- 测试/ipy_hello.py
+++ 测试/ipy_hello.py
@@ -1,20 +1,47 @@
 from IPy import IP
 
-ip = IP('183.192.8.0/22')
-ip_1 = IP('183.192.12.0/22')
-print(ip + ip_1)
-print(ip_1 + ip)
-print(ip.overlaps(ip_1), ip_1.overlaps(ip))
-exit()
-print(ip.len())
-print(ip)
-for item in ip:
-    print(item)
+"""
+将txt中的IP地址段 展示成 x.x.x.x ~ x.x.x.x的格式 方便合并
+"""
 
-print(ip.strNormal(0))
-print(ip.strNormal(1))
-print(ip.strNormal(2))
-print(ip.strNormal(3))
+filename = 'ipy_hello.txt'
+f = open(filename, 'r', newline='')
+reader = f.readlines()
 
-print(ip.iptype())
+for item in reader:
+    ip = IP(item)
+
+    print(ip.strNormal(0), '', end='')
+    # print(ip.strNormal(1))
+    # print(ip.strNormal(2))
+    # print(ip.strNormal(3))
+    length = ip.len() - 1
+    # print(length)
+    if length <= 255:
+        print('0.0.0.', end='')
+        print(length)
+    else:
+        length = int(((length + 1) / 256) - 1)
+        print('0.0.', end='')
+        print(length, end='')
+        print('.255')
+
+
+# ip = IP('183.192.8.0/22')
+# ip_1 = IP('183.192.12.0/22')
+# print(ip + ip_1)
+# print(ip_1 + ip)
+# print(ip.overlaps(ip_1), ip_1.overlaps(ip))
+# exit()
+# print(ip.len())
+# print(ip)
+# for item in ip:
+#     print(item)
+#
+# print(ip.strNormal(0))
+# print(ip.strNormal(1))
+# print(ip.strNormal(2))
+# print(ip.strNormal(3))
+#
+# print(ip.iptype())
 
